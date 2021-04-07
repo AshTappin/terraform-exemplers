a682e57ae8211d5388a809bd00b76e971d7cb49b
Ash <ash.tappin@blackpepper.co.uk>
2021-04-07 13:56:10 +0100

    Initial commit with basic Java app and react frontend

---
+++ server/src/main/java/com/blackpepper/terraform/terraformexemplers/TerraformExemplersApplication.java
@@ -0,0 +1,13 @@
+package com.blackpepper.terraform.terraformexemplers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformExemplersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraformExemplersApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/blackpepper/terraform/terraformexemplers/TerraformExemplersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackpepper.terraform.terraformexemplers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformExemplersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
