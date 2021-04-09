105bffbfc7ddedb83a2df261802058b93c3404bd
Ash <ash.tappin@blackpepper.co.uk>
2021-04-09 18:24:24 +0100

    Add workspace support

--- exemplars/azure/main.tf
+++ exemplars/azure/main.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "remote" {
     organization = "blackpeppersoftware"
-
     workspaces {
-      name = "ash"
+      prefix = "exemplar-"
     }
   }
 }
@@ -14,10 +13,19 @@ provider "azurerm" {
 
 locals {
   location = "UK South"
+  environment = lookup(var.environments, var.environment_key)
 }
 
 resource "azurerm_resource_group" "ash-exemplar" {
   location = local.location
-  name = "ash-exemplar-resource-group"
+  name = "exemplar-resource-group"
+}
+
+module "server" {
+  source = "./modules/server"
+  environment = local.environment
+  ip_restrictions = []
+  location = local.location
+  resource_group = azurerm_resource_group.ash-exemplar.name
 }
 
